f040f954913da2114776bd559a2601483031fc6a
Josh Jacobs <joshalexjacobs@gmail.com>
2025-01-24 23:46:11 -0500

    added logic that destroys bob

--- Assets/Scripts/BobaSlurper.cs
+++ Assets/Scripts/BobaSlurper.cs
@@ -1,12 +1,27 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using CandyCoded;
 using UnityEngine;
 
 public class BobaSlurper : MonoBehaviour {
+
+  [SerializeField] private float _distance = 5f;
+
+  [SerializeField] private LayerMask _bobaMask;
   
   private void Update() {
-    throw new NotImplementedException();
+    Vector3 direction = transform.TransformDirection(Vector3.down) * _distance;
+
+    RaycastHit hit;
+    
+    if (Physics.Raycast(transform.position, direction, out hit, _distance, _bobaMask)) {
+      Destroy(hit.transform.gameObject);
+    }
+  }
+
+  private void OnDrawGizmosSelected() {
+    Gizmos.color = Color.red;
+    Vector3 direction = transform.TransformDirection(Vector3.down) * _distance;
+    Gizmos.DrawRay(transform.position, direction);
   }
   
 }
