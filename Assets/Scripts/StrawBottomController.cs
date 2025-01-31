e9566a1869ba3b8e72f189ddf25d6b3b16d82174
Scott Doxey <neogeek0101@gmail.com>
2025-01-31 13:09:41 -0500

    Added boba collision logic.

--- Assets/Scripts/StrawBottomController.cs
+++ Assets/Scripts/StrawBottomController.cs
@@ -1,43 +1,73 @@
 using UnityEngine;
 
-namespace DefaultNamespace
+public class StrawBottomController : MonoBehaviour
 {
 
-    public class StrawBottomController : MonoBehaviour
+    private DrinkController _drinkController;
+
+    private const float _testRadius = 0.25f;
+
+    private const float _testDistance = 0.5f;
+
+    private readonly Collider[] _hitColliders = new Collider[10];
+
+    private Vector3 _testPosition => gameObject.transform.position - gameObject.transform.up * _testDistance;
+
+    private void Update()
     {
-
-        private DrinkController _drinkController;
-
-        private void OnTriggerEnter(Collider other)
+        if (Physics.OverlapSphereNonAlloc(_testPosition, _testRadius, _hitColliders, LayerMask.NameToLayer("Boba")) ==
+            0)
         {
-            if (!other.CompareTag("Tea"))
-            {
-                return;
-            }
-
-            if (_drinkController == null)
-            {
-                _drinkController = FindFirstObjectByType<DrinkController>();
-            }
-
-            _drinkController.StartingDrinking();
+            return;
         }
 
-        private void OnTriggerExit(Collider other)
+        foreach (var hit in _hitColliders)
         {
-            if (!other.CompareTag("Tea"))
+            if (hit == null || !hit.gameObject.CompareTag("Boba"))
             {
-                return;
+                continue;
             }
 
-            if (_drinkController == null)
-            {
-                _drinkController = FindFirstObjectByType<DrinkController>();
-            }
+            AudioManager.instance.Play(AudioManager.AudioClips.Slurp);
+            HapticsController.TriggerHapticFeedback();
 
-            _drinkController.StopDrinking();
+            Destroy(hit.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Tea"))
+        {
+            return;
         }
 
+        if (_drinkController == null)
+        {
+            _drinkController = FindFirstObjectByType<DrinkController>();
+        }
+
+        _drinkController.StartingDrinking();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Tea"))
+        {
+            return;
+        }
+
+        if (_drinkController == null)
+        {
+            _drinkController = FindFirstObjectByType<DrinkController>();
+        }
+
+        _drinkController.StopDrinking();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(_testPosition, _testRadius);
     }
 
 }
