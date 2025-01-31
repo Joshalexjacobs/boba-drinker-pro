da45f4bb9877f17660393f5b8fbdcf85d54f5735
Scott Doxey <neogeek0101@gmail.com>
2025-01-31 18:19:18 -0500

    Prevent running haptics in the editor.

--- Assets/Scripts/HapticsController.cs
+++ Assets/Scripts/HapticsController.cs
@@ -1,12 +1,10 @@
-using UnityEngine;
-
 public static class HapticsController
 {
 
     public static void TriggerHapticFeedback()
     {
-#if UNITY_WEBGL
-        Application.ExternalEval("if (navigator.vibrate) { navigator.vibrate(100); }");
+#if UNITY_WEBGL && !UNITY_EDITOR
+        UnityEngine.Application.ExternalEval("if (navigator.vibrate) { navigator.vibrate(100); }");
 #endif
     }
 
